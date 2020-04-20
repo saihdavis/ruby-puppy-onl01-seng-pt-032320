4b6c5bd9f5ed69ad4330a8469cea007266540a39
Saida Davis <saihdavis@gmail.com>
2020-04-20 16:21:50 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,16 @@
-# Add your code here
+class Dog 
+  
+attr_accessor :name
+
+  def initialize(name)
+    
+    @@all = []
+    
+  def self.all
+    @@all
+  end
+  
+    @@all << self 
+end
+
+end
